325d0f07f254bf7e98ccad6237649fa9f272e6cb
grandiloquent <1785907865@qq.com>
2023-04-22 12:04:26 +0800

    初始化项目

---
+++ app/src/main/java/psycho/euphoria/plane/MainActivity.java
@@ -0,0 +1,13 @@
+package psycho.euphoria.killer;
+ 
+public class MainActivity extends Activity {
+
+    static {
+/*
+加载编译Rust代码后得到共享库。它完整的名称为librust.so
+  */
+        System.loadLibrary("rust");
+    }
+
+   
+}
---
+++ app/src/main/rust/src/lib.rs
@@ -0,0 +1,6 @@
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn Java_psycho_euphoria_plane_MainActivity_startServer(
+    
+}
+
