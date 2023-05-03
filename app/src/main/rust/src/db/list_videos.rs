1c062a5ee2b95669a7e1e44d0be1ac5ecade008e
grandiloquent <1785907865@qq.com>
2023-05-04 03:39:52 +0800

    更新

--- app/src/main/rust/src/db/list_videos.rs
+++ app/src/main/rust/src/db/list_videos.rs
@@ -23,6 +23,26 @@ fn list_videos(
     }
     Ok(v)
 }
+fn list_videos_hidden(
+    conn: &MutexGuard<Connection>,
+    offset: u32,
+    limit: u32,
+) -> Result<Vec<Video>, rusqlite::Error> {
+    let mut query = conn.prepare("SELECT id,uri,title,image,source_type,update_at FROM video WHERE hidden >0 ORDER BY update_at DESC LIMIT ? OFFSET ?")?;
+    let mut rows = query.query(params![limit, offset])?;
+    let mut v = Vec::<Video>::new();
+    while let Some(row) = rows.next()? {
+        v.push(Video {
+            id: row.get(0)?,
+            uri: row.get(1)?,
+            title: row.get(2)?,
+            image: row.get(3)?,
+            source_type: row.get(4)?,
+            update_at: row.get(5)?,
+        });
+    }
+    Ok(v)
+}
 fn list_videos_by_source_type(
     conn: &MutexGuard<Connection>,
     offset: u32,
@@ -52,6 +72,12 @@ pub fn execute_list_videos(
 ) -> Result<String, Box<dyn Error>> {
     if t == 0 {
         let v = list_videos(conn, offset, limit)?;
+        match serde_json::to_string(&v) {
+            Ok(v) => Ok(v),
+            Err(_) => Err("")?,
+        }
+    } else if t >= 100 {
+        let v = list_videos_hidden(conn, offset, limit)?;
         match serde_json::to_string(&v) {
             Ok(v) => Ok(v),
             Err(_) => Err("")?,
